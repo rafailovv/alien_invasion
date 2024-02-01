48672882e6ad0792915cb44609e6099d423c554c
Arsen Rafailov <arsraf00@gmail.com>
2024-02-01 18:06:20 +0300

    Create game field and ship logic

---
+++ settings.py
@@ -0,0 +1,18 @@
+class Settings():
+    """ Класс для хранения всех настроек игры """
+
+    def __init__(self):
+        """ Инициализация настроек игры """
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+
+        # Настройки корабля
+        self.ship_speed = 1.5
+
+        # Настройки снаряда
+        self.bullet_speed = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
